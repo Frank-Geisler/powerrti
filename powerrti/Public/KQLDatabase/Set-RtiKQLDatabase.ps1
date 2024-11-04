1f60b12783a7907791c5e522ec51bb07a179a417
Frank Geisler <frank_geisler@geislers.net>
2024-11-04 22:44:04 +0100

    Added cmdlet Set-RtiKQLDatabase

---
+++ powerrti/Public/KQLDatabase/Set-RtiKQLDatabase.ps1
@@ -0,0 +1,73 @@
+function Set-RtiKQLDatabase {
+#Requires -Version 7.1
+
+<#
+.SYNOPSIS
+    Updates Properties of an existing Fabric KQLDatabase
+
+.DESCRIPTION
+    Updates Properties of an existing Fabric KQLDatabase
+
+.EXAMPLE
+    Set-RTIKQLDatabase 
+
+.LINK
+    https://learn.microsoft.com/en-us/rest/api/fabric/KQLDatabase/items/create-KQLDatabase?tabs=HTTP
+#>
+
+[CmdletBinding()]
+    param (
+
+        [Parameter(Mandatory=$true)]
+        [string]$WorkspaceId, 
+
+        [Parameter(Mandatory=$true)]
+        [string]$KQLDatabaseId, 
+        
+        [string]$KQLDatabaseName,
+
+        [string]$KQLDatabaseDescription
+
+    )
+
+begin {
+    # Check if session is established - if not throw error
+    if ($null -eq $RTISession.headerParams) {
+        throw "No session established to Fabric Real-Time Intelligence. Please run Connect-RTISession"
+    }
+
+    # Create body of request
+    $body = @{}
+
+    if ($PSBoundParameters.ContainsKey("KQLDatabaseName")) {
+        $body["displayName"] = $KQLDatabaseName
+    }
+
+    if ($PSBoundParameters.ContainsKey("KQLDatabaseDescription")) {
+        $body["description"] = $KQLDatabaseDescription
+    }
+
+    $body = $body `
+                | ConvertTo-Json `
+                    -Depth 1
+
+    # Create KQLDatabase API URL
+    $KQLDatabaseApiUrl = "$($RTISession.BaseFabricUrl)/v1/workspaces/$WorkspaceId/KQLDatabases/$KQLDatabaseId" 
+    }
+
+process {
+
+    # Call KQLDatabase API
+    $response = Invoke-RestMethod `
+                        -Headers $RTISession.headerParams `
+                        -Method PATCH `
+                        -Uri $KQLDatabaseApiUrl `
+                        -Body ($body) `
+                        -ContentType "application/json"
+
+    $response
+}
+
+end {}
+
+}
