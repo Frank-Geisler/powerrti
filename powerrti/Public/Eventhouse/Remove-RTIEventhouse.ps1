937a91597f229e2e44dbf9ee270b4fae6f330039
Frank Geisler <frank_geisler@geislers.net>
2024-11-04 20:25:24 +0100

    Removed unneccessary body code

--- powerrti/Public/Eventhouse/Remove-RTIEventhouse.ps1
+++ powerrti/Public/Eventhouse/Remove-RTIEventhouse.ps1
@@ -34,17 +34,10 @@ begin {
         throw "No session established to Fabric Real-Time Intelligence. Please run Connect-RTISession"
     }
 
-    # Create body of request
-    $body = @{
-    'displayName' = $EventhouseName
-    'description' = $EventhouseDescription
-    } | ConvertTo-Json `
-            -Depth 1
-
     # Create Eventhouse API URL
     $eventhouseApiUrl = "$($RTISession.BaseFabricUrl)/v1/workspaces/$WorkspaceId/eventhouses/$EventhouseId" 
 
-    }
+}
 
 process {
 
@@ -53,7 +46,6 @@ process {
                         -Headers $RTISession.headerParams `
                         -Method DELETE `
                         -Uri $eventhouseApiUrl `
-                        -Body ($body) `
                         -ContentType "application/json"
 
     $response
